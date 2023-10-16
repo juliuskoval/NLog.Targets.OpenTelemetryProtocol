536ca56002f9e1de5d00f3e59c1529c4155196b7
Julius Koval <julius.koval@absa.africa>
2023-10-16 17:20:46 +0200

    a

--- NLog.Targets.OpenTelemetryProtocol/GrpcLogExporter.cs
+++ NLog.Targets.OpenTelemetryProtocol/GrpcLogExporter.cs
@@ -17,12 +17,12 @@ namespace NLog.Targets.OpenTelemetryProtocol
 
             _channel = GrpcChannel.ForAddress(endpoint, grpcChannelOptions);
             _client = new LogsService.LogsServiceClient(_channel);
+            _headers.Add("user-agent", "OTel-OTLP-Exporter-Dotnet/1.6.1-alpha.0.54+865bcb64a5c67d713ded373a2839c49dba07dfc2");
+            _headers.Add("api-key", "eu01xx4ab133a94e4c5ab3b745eb05ee1802NRAL");
         }
 
         protected override void SendExportRequest(ExportLogsServiceRequest request)
         {
-            _headers.Add("user-agent", "OTel-OTLP-Exporter-Dotnet/1.6.1-alpha.0.54+865bcb64a5c67d713ded373a2839c49dba07dfc2");
-            // _headers.Add("api-key", "eu01xx4ab133a94e4c5ab3b745eb05ee1802NRAL");
             _client.Export(request, _headers);
         }
 
