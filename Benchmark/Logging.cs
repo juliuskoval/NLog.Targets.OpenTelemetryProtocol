28a99dbfed734ec347f660768337729731131a6b
Julius Koval <julius.koval@absa.africa>
2024-03-15 16:05:56 +0100

    enabling instrumentation scope

--- Benchmark/Logging.cs
+++ Benchmark/Logging.cs
@@ -4,12 +4,12 @@ using NLog;
 
 namespace Benchmark;
 
+[SimpleJob(RuntimeMoniker.Net48,baseline:true)]
 [SimpleJob(RuntimeMoniker.Net80)]
 [MemoryDiagnoser(false)]
 public class Logging
 {
     private readonly Logger _logger = LogManager.GetLogger("otlp");
-    private readonly Logger _loggerLightweight = LogManager.GetLogger("lightweight");
 
     [Benchmark]
     public void Log()
@@ -27,23 +27,5 @@ public class Logging
     public void Log2()
     {
         _logger.Info("{first} {second}", "message", "message2");
-    }
-
-    [Benchmark]
-    public void Log_Lightweight()
-    {
-        _loggerLightweight.Info("message");
-    }
-
-    [Benchmark]
-    public void Log1_lightweight()
-    {
-        _loggerLightweight.Info("{first}", "message");
-    }
-
-    [Benchmark]
-    public void Log2_Lightweight()
-    {
-        _loggerLightweight.Info("{first} {second}", "message", "message2");
     }
 }
